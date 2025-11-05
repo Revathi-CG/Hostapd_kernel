e573804e680d7a834f3e24be6440af2bd6069e5c
Revathi-CG <thota.revathi@capgemini.com>
2025-11-05 18:53:06 +0530

    Fixing implicit declaration of strtok_r() error - Created porting/string.h to provide a kernel-safe stub of strtok_r - The stub simply returns NULL and ignores its arguments - Allows hostapd user-space code to compile in kernel space without modifying original files - Updated porting/includes.h to include string.h

--- hostapd_driver/porting/string.h
+++ hostapd_driver/porting/string.h
@@ -17,6 +17,19 @@ static inline char *hostapd_strdup(const char *s)
     strcpy(dup, s);
     return dup;
 }
+/*
+ * Kernel-space stub for strtok_r to avoid hostapd build errors.
+ * Since strtok_r is user-space only, we provide a dummy function.
+ */
+
+static inline char *strtok_r(char *str, const char *delim, char **saveptr)
+{
+    /* Do nothing, just return NULL in kernel build */
+    (void)str;
+    (void)delim;
+    (void)saveptr;
+    return NULL;
+}
 
 /* Macro to replace os_strdup in kernel build */
 #define os_strdup(s) hostapd_strdup(s)
