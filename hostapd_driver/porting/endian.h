4590a294c23ca98285af9fa913159baf7dc40eed
Revathi-CG <thota.revathi@capgemini.com>
2025-11-05 15:16:30 +0530

    fixing byte_order,little_endian,big_endian not defined error Kernel has: 			#include <linux/byteorder/little_endian.h> 			#include <linux/byteorder/big_endian.h> But userspace code expects macros: 			__BYTE_ORDER 			__LITTLE_ENDIAN 			__BIG_ENDIAN Map kernel endian definitions to userspace style macros

--- hostapd_driver/porting/endian.h
+++ hostapd_driver/porting/endian.h
@@ -1,4 +1,27 @@
 #ifndef __ENDIAN_H_
 #define __ENDIAN_H_
 
+#include <linux/types.h>          // Needed for uint32_t etc.
+#include <asm/byteorder.h>        // Provides __LITTLE_ENDIAN__, __BIG_ENDIAN__
+
+/*
+ * User-space compatibility macros for hostapd
+ */
+
+#ifndef __LITTLE_ENDIAN
+#define __LITTLE_ENDIAN 1234
+#endif
+
+#ifndef __BIG_ENDIAN
+#define __BIG_ENDIAN 4321
+#endif
+
+#ifndef __BYTE_ORDER
+#ifdef __LITTLE_ENDIAN__
+#define __BYTE_ORDER __LITTLE_ENDIAN
+#else
+#define __BYTE_ORDER __BIG_ENDIAN
+#endif
+#endif
+
 #endif
