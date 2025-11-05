2c61e800de7555965388f702dcaa8de928e3af43
Revathi-CG <thota.revathi@capgemini.com>
2025-11-05 15:45:00 +0530

    fixing field v4,v6 has incomplete type add kernel networking header support for struct in_addr / in6_addr
    Hostapd expects <netinet/in.h> when compiled in user space, but when
    building inside the kernel, the symbols struct in_addr and struct in6_addr
    are undefined. This causes errors in src/utils/ip_addr.h:
    
        error: field ‘v4’ has incomplete type
        error: field ‘v6’ has incomplete type
    
    To avoid modifying upstream hostapd code, porting/includes.h is introduced
    and force-included via the Makefile. It includes the correct kernel headers:
    
        #include <linux/in.h>
        #include <linux/in6.h>
    
    This provides definitions for struct in_addr and struct in6_addr when
    building inside kernel space.
    
    No functional changes to hostapd code.

--- hostapd_driver/porting/includes.h
+++ hostapd_driver/porting/includes.h
@@ -1,6 +1,14 @@
 #ifndef __PORTING_INCLUDES_H_
 #define __PORTING_INCLUDES_H_
 
+/*
+	 * Ensure networking address structs (in_addr, in6_addr) exist before hostapd uses them.
+	 * Required when building hostapd in kernel space.
+	 */
+	#include <linux/in.h>      // struct in_addr
+	#include <linux/in6.h>     // struct in6_addr
+
+
 /* This file gets force-included via Makefile to override random_init() */
 #include "random.h"
 
