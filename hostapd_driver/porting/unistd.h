9560f6194b8f85af1d66b047282c026999757153
Revathi-CG <thota.revathi@capgemini.com>
2025-11-05 18:58:22 +0530

    Fixing implicit declaration of getopt() error - Created porting/unistd.h to provide a kernel-compatible stub of getopt() - getopt() always returns -1 (no options) to allow compilation in kernel space - Added stub variable  for compatibility with hostapd code - Allows hostapd to compile without modifying original source files

--- hostapd_driver/porting/unistd.h
+++ hostapd_driver/porting/unistd.h
@@ -1,4 +1,14 @@
 #ifndef __UNISTD_H_
 #define __UNISTD_H_
 
+/* Kernel-space stub for getopt */
+static inline int getopt(int argc, char * const argv[], const char *optstring)
+{
+    /* Stub: do nothing, always return -1 to indicate end of options */
+    return -1;
+}
+
+/* Provide stub for optind if code uses it */
+static int optind = 1;
+
 #endif
