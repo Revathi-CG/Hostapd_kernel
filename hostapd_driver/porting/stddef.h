f39db8a4976b63c089318fd8bedd14192cc304f1
Revathi-CG <thota.revathi@capgemini.com>
2025-11-05 12:51:52 +0530

    fixing NULL undeclared error
    Signed-off-by: Revathi-CG <thota.revathi@capgemini.com>

--- hostapd_driver/porting/stddef.h
+++ hostapd_driver/porting/stddef.h
@@ -1,6 +1,14 @@
 #ifndef __STDDEF_H_
 #define __STDDEF_H_
 
+#ifndef __SIZE_T_DEFINED
 typedef unsigned long size_t;
+#define __SIZE_T_DEFINED
+#endif
+
+/* NULL definition */
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
 
 #endif
