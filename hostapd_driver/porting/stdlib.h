00723975529bcd8b045cc24a3aaec77363c05555
Revathi-CG <thota.revathi@capgemini.com>
2025-11-05 13:01:15 +0530

    fixing malloc,realloc implicit declaration error
    Signed-off-by: Revathi-CG <thota.revathi@capgemini.com>

--- hostapd_driver/porting/stdlib.h
+++ hostapd_driver/porting/stdlib.h
@@ -1,4 +1,14 @@
 #ifndef __STDLIB_H_
 #define __STDLIB_H_
 
+#include <linux/slab.h>   // kmalloc, kfree, krealloc
+
+// Replace user-space realloc with kernel-space equivalent
+#define realloc(ptr, size) krealloc((ptr), (size), GFP_KERNEL)
+
+// Replace malloc and free too
+#define malloc(size) kmalloc((size), GFP_KERNEL)
+#define free(ptr) kfree(ptr)
+
+
 #endif
