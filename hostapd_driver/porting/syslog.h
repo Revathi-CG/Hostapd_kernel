6bd003fce8f51811ab9d21c4bb920c791103a488
Revathi-CG <thota.revathi@capgemini.com>
2025-11-05 15:57:17 +0530

    fixing LOG_DEBUG undeclared error Porting: Added syslog priority stubs for kernel build
    - Created porting/syslog.h
    - Defined LOG_DEBUG, LOG_INFO, LOG_ERR, etc. to avoid missing identifiers
    - Allowed hostapd logging API to compile in kernel without syslog

--- hostapd_driver/porting/syslog.h
+++ hostapd_driver/porting/syslog.h
@@ -1,5 +1,14 @@
 #ifndef __SYSLOG_H_
 #define __SYSLOG_H_
 
+#define LOG_EMERG   0
+#define LOG_ALERT   1
+#define LOG_CRIT    2
+#define LOG_ERR     3
+#define LOG_WARNING 4
+#define LOG_NOTICE  5
+#define LOG_INFO    6
+#define LOG_DEBUG   7
+
 #endif
 
