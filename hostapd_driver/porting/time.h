ced220d88cced402dd64492c1af4f95bce84ad6e
Revathi-CG <thota.revathi@capgemini.com>
2025-11-07 16:58:48 +0530

    Fixing implicit declaration of usleep() error - Added porting/time.h to redirect userspace usleep() call to kernel equivalents:     * usleep() → udelay() for short delays, usleep_range() for longer delays - Included time.h in porting/includes.h so hostapd source does not need modification - Resolves implicit declaration errors of usleep() when building hostapd in kernel space

--- hostapd_driver/porting/time.h
+++ hostapd_driver/porting/time.h
@@ -8,4 +8,13 @@ static inline void sleep(unsigned int sec) {
     ssleep(sec);
 }
 
+static inline void usleep(unsigned int usec)
+{
+    if (usec < 2000) {
+        udelay(usec);        // busy wait for short delays (<2ms)
+    } else {
+        usleep_range(usec, usec + 100); // proper sleep for longer delays
+    }
+}
+
 #endif /* __TIME_H_ */
