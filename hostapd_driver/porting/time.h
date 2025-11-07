ca27bf2628fc40b0b739be355a21bd9e9e0f6e69
Revathi-CG <thota.revathi@capgemini.com>
2025-11-07 16:46:07 +0530

    implicit declaration of sleep() error
    - Added time.h in porting/ to provide a kernel-compatible implementation of the userspace sleep() function.
    - sleep(sec) is redirected to ssleep(sec) to avoid implicit declaration errors when compiling hostapd in kernel space.
    - No changes made to hostapd source; all overrides are in the porting folder.

--- hostapd_driver/porting/time.h
+++ hostapd_driver/porting/time.h
@@ -1,4 +1,11 @@
 #ifndef __TIME_H_
 #define __TIME_H_
 
+#include <linux/delay.h>
+
+/* Replace userspace sleep(sec) with kernel sleep in seconds */
+static inline void sleep(unsigned int sec) {
+    ssleep(sec);
+}
+
 #endif /* __TIME_H_ */
