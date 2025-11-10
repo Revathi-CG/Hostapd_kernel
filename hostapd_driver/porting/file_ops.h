96a96b16c3815544600e9e36974c2c5cfae29ccd
Revathi-CG <thota.revathi@capgemini.com>
2025-11-10 14:59:30 +0530

    Fixing Implicit declaration of fread() Error Hostapd source expects libc fread(), which is not available in kernel space. Added fread() implementation in porting/file_ops.h that maps to kernel_read(),mirroring behavior of existing k_fopen(), k_fgets(), and k_fclose.
    This resolves:
      error: implicit declaration of function 'fread'
    
    Also defined FILE as struct file to ensure compatibility with kernel APIs.

--- hostapd_driver/porting/file_ops.h
+++ hostapd_driver/porting/file_ops.h
@@ -7,6 +7,8 @@
 #include <linux/kernel.h>   // kernel_read()
 #include <linux/uaccess.h>  // needed for file ops
 
+#define FILE struct file
+
 // fopen → kernel file open
 static inline FILE *k_fopen(const char *path, const char *mode)
 {
@@ -33,6 +35,20 @@ static inline int k_fclose(FILE *fp)
 {
     return filp_close(fp, NULL);
 }
+
+// fread → kernel_read() equivalent
+static inline size_t fread(void *ptr, size_t size, size_t nmemb, FILE *fp)
+{
+    loff_t pos = fp->f_pos;
+    ssize_t ret = kernel_read(fp, ptr, size * nmemb, &pos);
+
+    if (ret < 0)
+        return 0;
+
+    fp->f_pos = pos;
+    return ret;
+}
+
 
 #endif
 
