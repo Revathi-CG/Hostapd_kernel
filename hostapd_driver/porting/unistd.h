eea2bd16986df1da00191741d96e1bfa5491a011
Revathi-CG <thota.revathi@capgemini.com>
2025-11-10 13:03:09 +0530

    Fixing implicit declaration of getpid() error - getpid() is a userspace libc API and not available in the kernel - Added inline stub returning current->pid via task_struct - Avoids implicit declaration errors without modifying original hostapd sources

--- hostapd_driver/porting/unistd.h
+++ hostapd_driver/porting/unistd.h
@@ -14,4 +14,18 @@ static int optind  __attribute__((unused)) = 1;
 /* Stub for optarg in kernel space */
 static char *optarg  __attribute__((unused))= NULL;
 
+#ifndef __KERNEL_GETPID_STUB__
+#define __KERNEL_GETPID_STUB__
+
+/* Kernel equivalent of getpid() is task_pid_nr(current) */
+#include <linux/sched.h>
+
+static inline int getpid(void)
+{
+    return task_pid_nr(current);
+}
+
+#endif
+
+
 #endif
