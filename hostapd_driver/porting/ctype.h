d3382cb4470d2c44351f900faff59c64f0401abf
Revathi-CG <thota.revathi@capgemini.com>
2025-11-11 18:47:40 +0530

    Fixing implicit declaration of isblank() error - Implemented kernel_isblank() inline function - Macro redirect: isblank() -> kernel_isblank() - Allows hostapd ctrl_iface.c to compile without libc - Hostapd source code unchanged

--- hostapd_driver/porting/ctype.h
+++ hostapd_driver/porting/ctype.h
@@ -1,4 +1,16 @@
 #ifndef __CTYPE_H_
 #define __CTYPE_H_
 
+
+/* Kernel-space replacement for isblank() */
+/* Space ' ' and tab '\t' are considered blank */
+static inline int kernel_isblank(int c)
+{
+    return c == ' ' || c == '\t';
+}
+
+#define isblank(c) kernel_isblank(c)
+
+
+
 #endif
