0541b29feaaa499ea289d95ee0dc5372c6067559
Revathi-CG <thota.revathi@capgemini.com>
2025-11-11 18:06:16 +0530

    Fixing implicit declaration of hmac_sha1() error - Added kernel_hmac_sha1() using Linux kernel crypto_shash API - Redirected hmac_sha1() calls to kernel_hmac_sha1() via macro - Prevents implicit declaration errors in kernel build - Hostapd source code unchanged

--- hostapd_driver/porting/openssl/hmac.h
+++ hostapd_driver/porting/openssl/hmac.h
@@ -54,4 +54,54 @@ out:
 #define hmac_md5 kernel_hmac_md5
 
 
+#include <linux/kernel.h>
+
+/* Kernel-space HMAC-SHA1 implementation */
+static inline int kernel_hmac_sha1(const u8 *key, size_t key_len,
+                                   const u8 *data, size_t data_len,
+                                   u8 *digest)
+{
+    struct crypto_shash *tfm;
+    struct shash_desc *shash;
+    int ret;
+
+    tfm = crypto_alloc_shash("hmac(sha1)", 0, 0);
+    if (IS_ERR(tfm))
+        return PTR_ERR(tfm);
+
+    shash = kmalloc(sizeof(*shash) + crypto_shash_descsize(tfm), GFP_KERNEL);
+    if (!shash) {
+        crypto_free_shash(tfm);
+        return -ENOMEM;
+    }
+
+    shash->tfm = tfm;
+    // Modern kernels: no shash->flags
+
+    ret = crypto_shash_setkey(tfm, key, key_len);
+    if (ret)
+        goto out;
+
+    ret = crypto_shash_init(shash);
+    if (ret)
+        goto out;
+
+    ret = crypto_shash_update(shash, data, data_len);
+    if (ret)
+        goto out;
+
+    ret = crypto_shash_final(shash, digest);
+
+out:
+    kfree(shash);
+    crypto_free_shash(tfm);
+    return ret;
+}
+
+/* Redirect hostapd calls */
+#define hmac_sha1(key, key_len, data, data_len, digest) \
+        kernel_hmac_sha1(key, key_len, data, data_len, digest)
+
+
+
 #endif /* __HMAC_H_ */
