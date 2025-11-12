bc081c3a321071387537ab683fdda84914b3bf56
Revathi-CG <thota.revathi@capgemini.com>
2025-11-12 13:07:35 +0530

    Fixing implicit declaration of chmod() error - Implemented kernel_chmod() using kern_path() and notify_change() - Redirected all user-space chmod() calls via macro - Added backward-compatible #if for kernels < 6.3 - Prevented implicit declaration and argument mismatch errors - Enables hostapd control interface setup in kernel environment

--- hostapd_driver/porting/sys/stat.h
+++ hostapd_driver/porting/sys/stat.h
@@ -2,6 +2,9 @@
 #define __STAT_H_
 
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/namei.h>
+#include <linux/err.h>
 
 /*
  * Kernel-space stub for mkdir()
@@ -15,5 +18,40 @@ static inline int kernel_mkdir(const char *path, umode_t mode)
 
 /* Redirect all hostapd mkdir() calls to kernel stub */
 #define mkdir(path, mode) kernel_mkdir(path, mode)
+
+/*
+ * Minimal kernel-space replacement for chmod()
+ * Uses kern_path() + inode_permission() + notify_change()
+ */
+static inline int kernel_chmod(const char *filename, umode_t mode)
+{
+    struct path path;
+    struct iattr newattrs;
+    int err;
+
+    err = kern_path(filename, LOOKUP_FOLLOW, &path);
+    if (err)
+        return err;
+
+    newattrs.ia_mode = mode;
+    newattrs.ia_valid = ATTR_MODE;
+
+    inode_lock(path.dentry->d_inode);
+    #if LINUX_VERSION_CODE >= KERNEL_VERSION(6,3,0)
+    // new signature: notify_change(struct mnt_idmap *, struct dentry *, struct iattr *, struct inode **)
+    err = notify_change(mnt_idmap(path.mnt), path.dentry, &newattrs, NULL);
+#else
+    // old signature: notify_change(struct dentry *, struct iattr *, struct inode **)
+    err = notify_change(path.dentry, &newattrs, NULL);
+#endif
+    inode_unlock(path.dentry->d_inode);
+
+    path_put(&path);
+    return err;
+}
+
+/* Redirect all user-space chmod() calls to our kernel version */
+#define chmod(filename, mode) kernel_chmod(filename, mode)
+
 
 #endif /* __STAT_H_ */
