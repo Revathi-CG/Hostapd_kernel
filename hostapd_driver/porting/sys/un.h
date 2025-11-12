ee0041e0c96f10e47dbd276a5bd2817458c7391d
Revathi-CG <thota.revathi@capgemini.com>
2025-11-12 11:14:20 +0530

    Fixing kernel-space sockaddr_un definition Error
    - Created struct sockaddr_un in porting/sys/un.h
    - Allows hostapd to declare UNIX socket variables in kernel space
    - Does not require modifications to original hostapd sources

--- hostapd_driver/porting/sys/un.h
+++ hostapd_driver/porting/sys/un.h
@@ -1,4 +1,15 @@
 #ifndef __UN_H_
 #define __UN_H_
 
+#include <linux/kernel.h>
+#include <linux/socket.h>
+#include <linux/net.h>
+#include <linux/string.h>
+
+/* Kernel-space equivalent of sockaddr_un for porting */
+struct sockaddr_un {
+    sa_family_t sun_family;    /* AF_UNIX */
+    char sun_path[108];        /* maximum path */
+};
+
 #endif /* __UN_H_ */
