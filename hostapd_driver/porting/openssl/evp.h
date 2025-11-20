e1c759ca3869153bd3a6d797e015340d56ab955c
Revathi-CG <thota.revathi@capgemini.com>
2025-11-20 16:05:17 +0530

    Added stubs for OpenSSL EVP functions - Added dummy typedefs for BIGNUM, BN_CTX, and EVP_MD. - Added inline stub functions: BN_bin2bn, and openssl_digest_vector. - Prevents redefinition errors and implicit declaration warnings when building hostapd in kernel mode.

--- hostapd_driver/porting/openssl/evp.h
+++ hostapd_driver/porting/openssl/evp.h
@@ -1,4 +1,25 @@
 #ifndef __EVP_H_
 #define __EVP_H_
 
+#ifdef __KERNEL__
+
+/* Dummy OpenSSL types */
+typedef void BIGNUM;
+typedef void BN_CTX;
+typedef void EVP_MD;
+
+/* Stub functions returning NULL or 0 */
+static inline BIGNUM *BN_bin2bn(const unsigned char *s, int len, BIGNUM *ret)
+{
+    return NULL;
+}
+
+static inline int openssl_digest_vector(const EVP_MD *type, size_t num_elem,
+                                        const unsigned char *addr[], const size_t *len,
+                                        unsigned char *mac)
+{
+    return -1;
+}
+
+#endif
 #endif /* __EVP_H_ */
